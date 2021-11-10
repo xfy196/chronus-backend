66ed4576ba000084da09586791cbf4ba0672b80b
xfy196 <xufy@tianji.cn>
2021-11-10 16:44:24 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const process = require("child_process")
 
-const cmd = 'egg-scripts start --daemon --title=egg-server-chronus-backend'
+const cmd = 'npm run start'
 process.exec(cmd, function(error, stdout, stderr){
     console.log("error" + error)
     console.log("stdout" + stdout)
