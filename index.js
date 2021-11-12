b3f9cdd5698bb7ccafecd61d26c2e6d4912e01fa
xfy196 <xufy@tianji.cn>
2021-11-12 09:49:02 +0800

    update

--- index.js
+++
@@ -1,8 +0,0 @@
-const process = require("child_process")
-
-const cmd = 'npm i && npm run start'
-process.exec(cmd, function(error, stdout, stderr){
-    console.log("error" + error)
-    console.log("stdout" + stdout)
-    console.log("stderr" + stderr)
-})
